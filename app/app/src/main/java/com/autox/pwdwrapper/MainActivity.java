153c0cfb705b6afbe2d1c90222e99b1a13ea4f03
echo.xie <echo.xie@cootek.cn>
2020-03-25 10:08:02 +0800

    [feature]不用androidx

--- app/app/src/main/java/com/autox/pwdwrapper/MainActivity.java
+++ app/app/src/main/java/com/autox/pwdwrapper/MainActivity.java
@@ -1,8 +1,8 @@
 package com.autox.pwdwrapper;
 
-import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
